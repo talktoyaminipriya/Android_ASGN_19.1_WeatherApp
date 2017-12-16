1524563bb3847b508aa922978a5217a11c422b39
talktoyaminipriya <talktoyaminipriya@gmail.com>
2017-12-17 01:03:45 +0530

    Update Function.java

--- app/src/main/java/com/example/priya/weatherapp/Function.java
+++ app/src/main/java/com/example/priya/weatherapp/Function.java
@@ -20,8 +20,8 @@ import java.util.Locale;
 
 public class Function {
 
-    // Project Created by Ferdousur Rahman Shajib
-    // www.androstock.com
+    
+    
 
     private static final String OPEN_WEATHER_MAP_URL =
             "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
